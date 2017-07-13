9b69ed6d6eca8b6c5ac270b5a524f98d12e57101
Petra Bakosova <petrab@chicagobooth.edu>
2017-07-13 01:34:01 +0000

    first row more or less done

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -7,4 +7,37 @@ class CalculationsController < ApplicationController
       
       render ("calculations/flex_square.html.erb") 
    end
+   
+    def flex_root
+      
+      @number = params["root_number"].to_i
+      @squared_root =  Math.sqrt(@number)
+      
+      render ("calculations/flex_root.html.erb") 
+   end
+   
+    def flex_payment
+      
+      @apr = params["apr"].to_f
+      @years = params["years"].to_i
+      @loan = params["loan"].to_i
+      
+      total = @loan * (1+@apr/10000)**@years
+      r =  @apr/10000/12
+      num = r * @loan 
+      den = 1 - (1 + r)**(- @years*12) 
+      @monthly_payment = num / den
+    
+    render ("calculations/flex_payment.html.erb") 
+   end
+   
+    def flex_random
+      
+      @from = params["from"].to_i
+      @to = params["to"].to_i
+      range = @to - @from + 1
+      @random_number = rand(range) + @from 
+      
+      render ("calculations/flex_random.html.erb") 
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
   get("/flexible/square/:a_number", { :controller => "calculations", :action => "flex_square"})
-  
+  get("/flexible/square_root/:root_number", { :controller => "calculations", :action => "flex_root"})
+  get("/flexible/payment/:apr/:years/:loan", { :controller => "calculations", :action => "flex_payment"})
+  get("/flexible/random/:from/:to", { :controller => "calculations", :action => "flex_random"})
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
 end
